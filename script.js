e6af0cbdc12643ca3d59f63c8bc326c72eca25a7
MatheuMa <matheumazx@outlook.com>
2026-05-26 17:29:39 +1000

    Compete all functions. Need refactoring.

--- script.js
+++ script.js
@@ -7,17 +7,12 @@ let operator = '';
 let numInput2 = '';
 let equalsFlag = false;
 
-btns.addEventListener('click', (e) => {
-    let target = e.target;
+document.addEventListener('keydown', (e) => {
+    let keyName = e.key;
 
-    switch (target.id) {
-        case 'back':
-            if (digits.textContent.length > 0) {
-                digits.textContent = digits.textContent.slice(0, -1);
-            }
-            break;
-        case 'clear':
-            clear();
+    switch(keyName) {
+        case 'Backspace':
+            handleBack();
             break;
         case '+':
         case '-':
@@ -28,50 +23,39 @@ btns.addEventListener('click', (e) => {
                 break;
             }
 
+            // get num1 from the input            
             if (numInput1.length === 0) {
                 numInput1 = digits.textContent;
                 last.textContent = numInput1;
                 digits.textContent = "";
             }
 
-            if (Number(digits.textContent) === 0 && operator === '/') {
-                alert("Divide by 0 error");
+            if (dividedBy0()) {
+                alert("Nice try. The universe still refuses to divide by zero.");
                 clear();
                 break;
             }
             
             if (digits.textContent.length > 0) {
                 // if input as num2 has a value
-                numInput2 = digits.textContent;
-                let result = operate(numInput1, operator, numInput2);
-                numInput1 = result;
-                last.textContent = numInput1;
-                digits.textContent = "";
+                calculateResult();
             }
             
-            operator = target.id;
+            operator = keyName;
             last.textContent += operator;
             break;
         case '=':
-            if (Number(digits.textContent) === 0 && operator === '/') {
-                alert("Nice try. The universe still refuses to divide by zero.");
-                clear();
-                break;
-            }
-
+        case 'Enter':
+            e.preventDefault();
             if (numInput1 && operator.length > 0 && digits.textContent.length > 0) {
-                numInput2 = digits.textContent;
-                digits.textContent = '';
-                let result = operate(numInput1, operator, numInput2);
-                numInput1 = result;
-                last.textContent = numInput1;
-                equalsFlag = true;
-            }
+                if (dividedBy0()) {
+                    alert("Nice try. The universe still refuses to divide by zero.");
+                    clear();
+                    break;
+                }
 
-            break;
-        case '.':
-            if (!digits.textContent.includes('.')) {
-                input('.');
+                calculateResult();
+                equalsFlag = true;
             }
             break;
         case '0':
@@ -85,14 +69,124 @@ btns.addEventListener('click', (e) => {
             }
             input('0');
             break;
-        default:
-        if (equalsFlag) {
-            equalsFlag = false;
-            clear();
-        }
-        input(target.id);
+        case '.':
+            decimalInput();
+            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            handleDefaultInput(keyName);
     }
 })
+
+btns.addEventListener('click', (e) => {
+    let target = e.target;
+
+    switch(target.id) {
+        case 'back':
+            handleBack();
+            break;
+        case 'clear':
+            clear();
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            // check if num1 has a value first
+            if (digits.textContent === '') {
+                break;
+            }
+
+            // get num1 from the input            
+            if (numInput1.length === 0) {
+                numInput1 = digits.textContent;
+                last.textContent = numInput1;
+                digits.textContent = "";
+            }
+
+            if (dividedBy0()) {
+                alert("Nice try. The universe still refuses to divide by zero.");
+                clear();
+                break;
+            }
+            
+            if (digits.textContent.length > 0) {
+                // if input as num2 has a value
+                calculateResult();
+            }
+            
+            operator = target.id;
+            last.textContent += operator;
+            break;
+        case '=':
+            if (numInput1 && operator.length > 0 && digits.textContent.length > 0) {
+                if (dividedBy0()) {
+                    alert("Nice try. The universe still refuses to divide by zero.");
+                    clear();
+                    break;
+                }
+
+                calculateResult();
+                equalsFlag = true;
+            }
+            break;
+        case '.':
+            decimalInput();
+            break;
+        case '0':
+            if (equalsFlag) {
+                equalsFlag = false;
+                clear();
+            }
+
+            if (digits.textContent === '0') {
+                break;
+            }
+            input('0');
+            break;
+        default:
+            handleDefaultInput(target.id);
+    }
+})
+
+function handleBack() {
+    if (digits.textContent.length > 0) {
+        digits.textContent = digits.textContent.slice(0, -1);
+    }
+}
+
+function dividedBy0() {
+    return Number(digits.textContent) === 0 && operator === '/';
+}
+
+function calculateResult() {
+    numInput2 = digits.textContent;
+    digits.textContent = '';
+    let result = operate(numInput1, operator, numInput2);
+    numInput1 = result;
+    last.textContent = numInput1;
+}
+
+function decimalInput() {
+    if (!digits.textContent.includes('.')) {
+        input('.');
+    }
+}
+
+function handleDefaultInput(value) {
+    if (equalsFlag) {
+        equalsFlag = false;
+        clear();
+    }
+    input(value);
+}
 
 function operate(num1, operator, num2) {
     let result;
